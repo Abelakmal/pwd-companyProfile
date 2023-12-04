cbb5eb81f84cb0368047a44dea8618d63c692995
Abelakmal <abelakmal06@gmail.com>
2023-12-05 01:03:01 +0700

    menambahkan metadata

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Engineers & Contractors',
   description: 'Make by Company Engineers & Contractors',
+  openGraph: {
+    title: 'Engineers & Contractors',
+    description: 'Make by Company Engineers & Contractors',
+    image: 'https://www.usnews.com/object/image/00000184-0b98-d844-a5ac-bbbf25520000/gettyimages-1352673708.jpg'
+  }
 };
 
 export default function RootLayout({ children }) {
